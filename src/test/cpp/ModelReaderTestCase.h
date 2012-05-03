645edeff14efefb6e8171f330ab3f8160b319ce9
Far McKon <FarMcKon@gmail.com>
2012-05-03 17:21:45 -0400

    another update for ModelReader test cases

--- src/test/cpp/ModelReaderTestCase.h
+++ src/test/cpp/ModelReaderTestCase.h
@@ -13,27 +13,11 @@
 class ModelReaderTestCase : public CPPUNIT_NS::TestFixture
 {
   CPPUNIT_TEST_SUITE( ModelReaderTestCase );
-//	  CPPUNIT_TEST( testLayerSplit );
-//	  CPPUNIT_TEST( testSlicySimple );
 
-	  CPPUNIT_TEST( testMeshyCycleNull );
-  	  CPPUNIT_TEST( testMeshyCycleMin );
-	  CPPUNIT_TEST( testMeshyCycle );
-////	  CPPUNIT_TEST( testMeshyLoad );
-//// //	  CPPUNIT_TEST( testLargeMeshy );
-////	  CPPUNIT_TEST( testSlicyWater );
-//
-//	  CPPUNIT_TEST( testTubularInflate);
-//	  CPPUNIT_TEST( testCutTriangle);
-//
-//	  /// //	  CPPUNIT_TEST( testRotate);
-//	  CPPUNIT_TEST( testInputStls );
-//// //	  CPPUNIT_TEST( fixContourProblem );
-//	  CPPUNIT_TEST( testLayerMeasure );
-//// //	  CPPUNIT_TEST( testMyStls );
-//
-//	  CPPUNIT_TEST( testMeshySimple );
-//	  CPPUNIT_TEST( testKnot);
+  CPPUNIT_TEST( testAsciiStlFormatterCycleNull );
+  CPPUNIT_TEST( testAsciiStlFormatterCycleMin );
+  CPPUNIT_TEST( testAsciiStlFormatterCycle );
+
   CPPUNIT_TEST_SUITE_END();
 
 
@@ -42,30 +26,10 @@ public:
 
 protected:
 
-  void testMeshyCycleNull();
-  void testMeshyCycleMin();
-  void testMeshyCycle();
+  void testAsciiStlFormatterCycleNull();
+  void testAsciiStlFormatterCycleMin();
+  void testAsciiStlFormatterCycle();
 
-//  void testSlicySimple();
-//
-//  void testLayerSplit();
-//  void testMeshySimple();
-//
-//  void testLayerMeasure();
-//
-//  void testMeshyLoad();
-//  void testLargeMeshy();
-//  void testSlicyWater();
-//
-//  // cutting a triangle along z line
-//  void testCutTriangle();
-//  // rotation nad translation of 2D points
-//  void testRotate();
-//  void testTubularInflate();
-//  void testInputStls();
-//  void testMyStls();
-//  void fixContourProblem();
-//  void testKnot();
 };
 
 
